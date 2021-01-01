1b725841881d5e72bdb81a772e1b2f6d3a1d2d98
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-01-01 17:36:45 +0100

    Implement helper functions for Spotify OAuth flow

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/zmb3/spotify"
+)
 
 func main() {
 	fmt.Println("admirer")
+}
+
+func createSpotifyAuthenticator() spotify.Authenticator {
+	clientID := os.Getenv("SPOTIFY_CLIENT_ID")
+	clientSecret := os.Getenv("SPOTIFY_CLIENT_SECRET")
+
+	if len(clientID) == 0 || len(clientSecret) == 0 {
+		panic("Please set SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET environment variables.")
+	}
+
+	// Not an actual web server (yet).
+	redirectURL := "https://admirer.test"
+	authenticator := spotify.NewAuthenticator(redirectURL, spotify.ScopeUserReadPrivate)
+	authenticator.SetAuthInfo(clientID, clientSecret)
+
+	return authenticator
+}
+
+func createSpotifyAuthURL(authenticator *spotify.Authenticator) string {
+	return authenticator.AuthURL("")
+}
+
+func callbackSpotify(authenticator *spotify.Authenticator, code string) spotify.Client {
+	token, err := authenticator.Exchange(code)
+	if err != nil {
+		panic("Failed to parse Spotify token.")
+	}
+
+	return authenticator.NewClient(token)
 }

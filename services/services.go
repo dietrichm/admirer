8cd708067e2b564952c7e1b8f60aa10dabe57459
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-01-02 14:52:18 +0100

    Create factory `ForName` to instantiate service by name

--- services/services.go
+++ services/services.go
@@ -1,8 +1,25 @@
 package services
 
+import (
+	"github.com/dietrichm/admirer/services/lastfm"
+	"github.com/dietrichm/admirer/services/spotify"
+)
+
 // Service is the external service interface.
 type Service interface {
 	CreateAuthURL() string
 	Authenticate(code string)
 	GetUsername() string
+}
+
+// ForName returns service instance for service name.
+func ForName(serviceName string) Service {
+	switch serviceName {
+	case "spotify":
+		return spotify.NewSpotify()
+	case "lastfm":
+		return lastfm.NewLastfm()
+	default:
+		panic("Unknown service " + serviceName)
+	}
 }

a6a3bc0603081d0365ab0ff1a22bae90b1b0a542
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-01-02 12:17:10 +0100

    Instantiate Last.fm API client

--- services/lastfm/lastfm.go
+++ services/lastfm/lastfm.go
@@ -1,5 +1,23 @@
 package lastfm
 
+import (
+	"os"
+
+	lastfm_api "github.com/shkh/lastfm-go/lastfm"
+)
+
 // Login runs the CLI procedure for logging in on Last.fm.
 func Login(oauthCode string) {
+	apiClient := createAPIClient()
+}
+
+func createAPIClient() *lastfm_api.Api {
+	clientID := os.Getenv("LASTFM_CLIENT_ID")
+	clientSecret := os.Getenv("LASTFM_CLIENT_SECRET")
+
+	if len(clientID) == 0 || len(clientSecret) == 0 {
+		panic("Please set LASTFM_CLIENT_ID and LASTFM_CLIENT_SECRET environment variables.")
+	}
+
+	return lastfm_api.New(clientID, clientSecret)
 }

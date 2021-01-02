2d99e148898503bd9cf5f3db5ad0d25b2b15f179
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-01-02 20:16:58 +0100

    Let Cobra handle CLI commands

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"flag"
-	"os"
-
 	"github.com/dietrichm/admirer/commands"
 )
 
 func main() {
-	var loginServiceName string
-	flag.StringVar(&loginServiceName, "login", "", "Log in on external service")
-
-	var oauthCode string
-	flag.StringVar(&oauthCode, "oauth-code", "", "OAuth code")
-
-	flag.Parse()
-
-	if len(loginServiceName) != 0 {
-		commands.Login(loginServiceName, oauthCode)
-		os.Exit(0)
-	}
-
-	flag.Usage()
-	os.Exit(1)
+	commands.Execute()
 }

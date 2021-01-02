fcf50af90a70b2153956bdcf6c191fd9352315d7
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-01-02 19:53:19 +0100

    Create basic root Cobra command

--- commands/commands.go
+++ commands/commands.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/dietrichm/admirer/services"
+	"github.com/spf13/cobra"
 )
 
 // Login runs the command for logging in on an external service.
@@ -18,4 +19,17 @@ func Login(serviceName string, oauthCode string) {
 	service.Authenticate(oauthCode)
 
 	fmt.Println("Logged in on " + service.Name() + " as " + service.GetUsername())
+}
+
+var rootCommand = &cobra.Command{
+	Use:   "admirer",
+	Short: "A command line utility to sync song likes between Spotify and Last.fm.",
+}
+
+// Execute runs the requested CLI command.
+func Execute() {
+	err := rootCommand.Execute()
+	if err != nil {
+		panic(err)
+	}
 }

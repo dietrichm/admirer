41d608a214c685c9cc89ea06bc4d965a9f36ddb3
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-03-25 12:06:32 +0100

    Define and use available services as `MapServiceLoader`

--- services/services.go
+++ services/services.go
@@ -2,10 +2,25 @@
 
 package services
 
+import (
+	"github.com/dietrichm/admirer/services/lastfm"
+	"github.com/dietrichm/admirer/services/spotify"
+)
+
 // Service is the external service interface.
 type Service interface {
 	Name() string
 	CreateAuthURL() string
 	Authenticate(code string) error
 	GetUsername() (string, error)
+}
+
+// AvailableServices is the configured ServiceLoader for the available services.
+var AvailableServices = MapServiceLoader{
+	"spotify": func() (Service, error) {
+		return spotify.NewSpotify()
+	},
+	"lastfm": func() (Service, error) {
+		return lastfm.NewLastfm()
+	},
 }

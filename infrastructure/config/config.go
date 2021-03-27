b94470f32a5d7df4c7c621226d1735000a8ddb79
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-03-27 17:03:48 +0100

    Load Last.fm access token (session key) from secrets file

--- infrastructure/config/config.go
+++ infrastructure/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"io/fs"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/viper"
 )
@@ -10,6 +11,12 @@ import (
 // Config is the interface for reading and writing configuration.
 type Config interface {
 	GetString(key string) string
+}
+
+// LoadConfig creates a Config struct for reading and writing configuration.
+func LoadConfig(name string) (Config, error) {
+	filename := filepath.Join(os.Getenv("HOME"), ".config", "admirer", name)
+	return loadConfigFromFile(filename)
 }
 
 func loadConfigFromFile(filename string) (Config, error) {

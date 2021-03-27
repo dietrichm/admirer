96a515ce40fc31541b2d6621c82355b9f4ff2f65
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-03-27 15:39:18 +0100

    Assert it reads YAML configuration file

--- infrastructure/config/config_test.go
+++ infrastructure/config/config_test.go
@@ -3,7 +3,34 @@ package config
 import (
 	"io/ioutil"
 	"os"
+	"testing"
 )
+
+func TestConfig(t *testing.T) {
+	t.Run("reads YAML configuration file", func(t *testing.T) {
+		file, err := createFile("foo: bar")
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		defer os.Remove(file.Name())
+
+		config, err := loadConfigFromFile(file.Name())
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+
+		if config == nil {
+			t.Fatal("Expected config struct")
+		}
+
+		expected := "bar"
+		got := config.GetString("foo")
+		if got != expected {
+			t.Errorf("expected %q, got %q", expected, got)
+		}
+	})
+}
 
 func createFile(contents string) (*os.File, error) {
 	file, err := ioutil.TempFile(os.TempDir(), "admirer-")

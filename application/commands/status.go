34b40d18274f9839e6a77e820d0899b94b0af400
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-03-31 21:46:18 +0200

    Print service display names in status command

--- application/commands/status.go
+++ application/commands/status.go
@@ -1,6 +1,11 @@
 package commands
 
 import (
+	"fmt"
+	"io"
+
+	"github.com/dietrichm/admirer/domain"
+	"github.com/dietrichm/admirer/infrastructure/services"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +17,19 @@ var statusCommand = &cobra.Command{
 	Use:   "status",
 	Short: "Retrieve status for services",
 	RunE: func(command *cobra.Command, args []string) error {
-		return nil
+		return status(services.AvailableServices, command.OutOrStdout())
 	},
+}
+
+func status(serviceLoader domain.ServiceLoader, writer io.Writer) error {
+	for _, serviceName := range serviceLoader.Names() {
+		service, err := serviceLoader.ForName(serviceName)
+		if err != nil {
+			return err
+		}
+
+		fmt.Fprintln(writer, service.Name())
+	}
+
+	return nil
 }
--- application/commands/status_test.go
+++ application/commands/status_test.go
@@ -1,8 +1,45 @@
 package commands
 
 import (
+	"bytes"
 	"testing"
+
+	"github.com/dietrichm/admirer/domain"
+	"github.com/golang/mock/gomock"
 )
 
 func TestStatus(t *testing.T) {
+	t.Run("returns status for each service", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+
+		fooService := domain.NewMockService(ctrl)
+		fooService.EXPECT().Name().Return("Foo")
+
+		barService := domain.NewMockService(ctrl)
+		barService.EXPECT().Name().Return("Bar")
+
+		serviceLoader := domain.NewMockServiceLoader(ctrl)
+		serviceLoader.EXPECT().Names().Return([]string{"foo", "bar"})
+		serviceLoader.EXPECT().ForName("foo").Return(fooService, nil)
+		serviceLoader.EXPECT().ForName("bar").Return(barService, nil)
+
+		expected := `Foo
+Bar
+`
+		got, err := executeStatus(serviceLoader)
+
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+
+		if got != expected {
+			t.Errorf("expected %q, got %q", expected, got)
+		}
+	})
+}
+
+func executeStatus(serviceLoader domain.ServiceLoader) (string, error) {
+	buffer := new(bytes.Buffer)
+	err := status(serviceLoader, buffer)
+	return buffer.String(), err
 }

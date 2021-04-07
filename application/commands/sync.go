c5124e338b7cb72398e3222c22af02fa96e5b359
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-04-07 21:22:59 +0200

    Love tracks on target service returned from source service

--- application/commands/sync.go
+++ application/commands/sync.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/dietrichm/admirer/domain"
@@ -22,5 +23,22 @@ var syncCommand = &cobra.Command{
 }
 
 func sync(serviceLoader domain.ServiceLoader, writer io.Writer, args []string) error {
+	sourceServiceName := args[0]
+	targetServiceName := args[1]
+
+	sourceService, _ := serviceLoader.ForName(sourceServiceName)
+	targetService, _ := serviceLoader.ForName(targetServiceName)
+
+	defer sourceService.Close()
+	defer targetService.Close()
+
+	tracks, _ := sourceService.GetLovedTracks(10)
+
+	for _, track := range tracks {
+		targetService.LoveTrack(track)
+
+		fmt.Fprintln(writer, "Synced:", track.String())
+	}
+
 	return nil
 }
--- application/commands/sync_test.go
+++ application/commands/sync_test.go
@@ -5,9 +5,50 @@ import (
 	"testing"
 
 	"github.com/dietrichm/admirer/domain"
+	"github.com/golang/mock/gomock"
 )
 
 func TestSync(t *testing.T) {
+	t.Run("loves tracks on target service returned from source service", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+
+		trackOne := domain.Track{
+			Artist: "Awesome Artist",
+			Name:   "Blam (Instrumental)",
+		}
+		trackTwo := domain.Track{
+			Artist: "Foo & Bar",
+			Name:   "Mr. Testy",
+		}
+		tracks := []domain.Track{trackOne, trackTwo}
+
+		sourceService := domain.NewMockService(ctrl)
+		sourceService.EXPECT().GetLovedTracks(10).Return(tracks, nil)
+		sourceService.EXPECT().Close()
+
+		targetService := domain.NewMockService(ctrl)
+		targetService.EXPECT().LoveTrack(trackOne).Return(nil)
+		targetService.EXPECT().LoveTrack(trackTwo).Return(nil)
+		targetService.EXPECT().Close()
+
+		serviceLoader := domain.NewMockServiceLoader(ctrl)
+		serviceLoader.EXPECT().ForName("source").Return(sourceService, nil)
+		serviceLoader.EXPECT().ForName("target").Return(targetService, nil)
+
+		got, err := executeSync(serviceLoader, "source", "target")
+
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+
+		expected := `Synced: Awesome Artist - Blam (Instrumental)
+Synced: Foo & Bar - Mr. Testy
+`
+
+		if got != expected {
+			t.Errorf("expected %q, got %q", expected, got)
+		}
+	})
 }
 
 func executeSync(serviceLoader domain.ServiceLoader, args ...string) (string, error) {

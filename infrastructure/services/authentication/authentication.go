b34e962d18fabc3fb1cf10841f97c77af2e60741
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-04-22 11:55:12 +0200

    Generate mock CallbackProvider

--- infrastructure/services/authentication/authentication.go
+++ infrastructure/services/authentication/authentication.go
@@ -1,3 +1,5 @@
+//go:generate mockgen -source authentication.go -destination authentication_mock.go -package authentication
+
 package authentication
 
 // DefaultCallbackProvider is the default callback provider for services.

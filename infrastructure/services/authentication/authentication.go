da51a0cbcb5674aa6b7409008f1ae50b89bfd620
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-04-22 11:54:11 +0200

    Instantiate `httpCallbackProvider` as default callback provider

--- infrastructure/services/authentication/authentication.go
+++ infrastructure/services/authentication/authentication.go
@@ -1,4 +1,7 @@
 package authentication
 
+// DefaultCallbackProvider is the default callback provider for services.
+var DefaultCallbackProvider = &httpCallbackProvider{}
+
 // CallbackProvider provides a callback mechanism for authenticating services.
 type CallbackProvider interface{}

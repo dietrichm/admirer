cd220e852e5178bcd28816c224c3f204905cd02f
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-04-23 09:25:41 +0200

    Have dummy `ReadCode` implementation for HTTP provider

--- infrastructure/authentication/http.go
+++ infrastructure/authentication/http.go
@@ -1,3 +1,9 @@
 package authentication
 
+import "io"
+
 type httpCallbackProvider struct{}
+
+func (h httpCallbackProvider) ReadCode(key string, writer io.Writer) (code string, err error) {
+	return
+}

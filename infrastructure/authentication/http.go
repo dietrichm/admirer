81a70d5752d5b1dd68ebb7c7aa60f4048d0fbdbd
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-04-23 09:33:59 +0200

    Have dummy HTTP callback handler

--- infrastructure/authentication/http.go
+++ infrastructure/authentication/http.go
@@ -1,9 +1,17 @@
 package authentication
 
-import "io"
+import (
+	"io"
+	"net/http"
+)
 
 type httpCallbackProvider struct{}
 
 func (h httpCallbackProvider) ReadCode(key string, writer io.Writer) (code string, err error) {
 	return
+}
+
+type httpCallbackHandler struct{}
+
+func (h httpCallbackHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 }
--- infrastructure/authentication/http_test.go
+++ infrastructure/authentication/http_test.go
@@ -1,8 +1,17 @@
 package authentication
 
 import (
+	"net/http/httptest"
 	"testing"
 )
 
 func TestHttpCallbackProvider(t *testing.T) {
+}
+
+func TestHttpCallbackHandler(t *testing.T) {
+	request := httptest.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+
+	handler := new(httpCallbackHandler)
+	handler.ServeHTTP(response, request)
 }
